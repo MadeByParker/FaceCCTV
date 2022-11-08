2f64abaff5673a2139b070618c16040f009477a3
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-11-08 01:22:08 +0000

    Create forge.config.js

---
+++ Desktop/app/forge.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: "FaceCCTV",
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
