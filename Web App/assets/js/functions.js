0217814c977f34d77f41f68f3f7a3a7cbda4ca2f
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-11-10 00:39:36 +0000

    added sidebar

--- Web App/assets/js/functions.js
+++ Web App/assets/js/functions.js
@@ -73,3 +73,22 @@ document.addEventListener("click", (event) => {
 		}
 	}
 });
+
+closeBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("open");
+  menuBtnChange();//calling the function(optional)
+});
+
+searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+ if(sidebar.classList.contains("open")){
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+ }else {
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+ }
+}
--- Web App/assets/js/page-assets.js
+++ Web App/assets/js/page-assets.js
@@ -10,3 +10,7 @@ let divAppContent = document.getElementById("app-content");
 let DOMCache = document.getElementById("dom-cache");
 
 let divLoading = document.getElementById("loading-overlay");
+
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
