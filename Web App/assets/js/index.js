7428b385b4b3ec6c0e763a59ea8c36e97969ad01
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-11-15 02:09:49 +0000

    added dark mode and container for body

--- Web App/assets/js/index.js
+++ Web App/assets/js/index.js
@@ -108,3 +108,23 @@ function dropDown() {
     }
   }
   
+  const btn = document.getElementById("mode-switch");
+  btn.addEventListener("click", () => { document.html.classList.toggle("dark");
+  });
+
+  
+// The body element's ID is set to mobile or desktop depending on what device the user is on (based on the user agent).
+detectMobile() ? document.body.id = "mobile" : document.body.id = "desktop";
+
+// The app's platform ("web" or "app") depends on the ID of the document element.
+const appPlatform = document.documentElement.id;
+
+// In order to have the same codebase across the web and desktop app, there are a few empty variables that are only given a value when the app platform is set to "app".
+let electron = null;
+let ipcRenderer = null;
+
+if(appPlatform !== "app" && typeof require === "undefined") {
+	var require = () => { 
+		return "";
+	};
+}
