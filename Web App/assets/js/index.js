6b2cdd1577ab1186e39fa2989c578db0b6881cbf
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-11-17 10:20:19 +0000

    fixed dark mode

--- Web App/assets/js/index.js
+++ Web App/assets/js/index.js
@@ -108,9 +108,6 @@ function dropDown() {
     }
   }
   
-  const btn = document.getElementById("mode-switch");
-  btn.addEventListener("click", () => { document.html.classList.toggle("dark");
-  });
 
   
 // The body element's ID is set to mobile or desktop depending on what device the user is on (based on the user agent).
@@ -127,4 +124,64 @@ if(appPlatform !== "app" && typeof require === "undefined") {
 	var require = () => { 
 		return "";
 	};
+}
+
+let defaultSettings = {
+	"theme": "light"
+};
+
+const container = document.querySelector(".container");
+const toggle = document.querySelector(".toggle");
+
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
+});
+
+
+
+if (
+  localStorage.getItem('color-theme') === 'dark' ||
+  (!('color-theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
 }
