73da1eee42e4bda074d6dffb2dac391dca1d348a
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-11-17 10:20:35 +0000

    set dark mode

---
+++ Web App/assets/js/variables.js
@@ -0,0 +1,38 @@
+let buttonWindowClose = document.getElementById("button-window-close");
+let buttonWindowMinimize = document.getElementById("button-window-minimize");
+let buttonWindowMaximize = document.getElementById("button-window-maximize");
+
+let appContainer = document.getElementById("app-container");
+
+let DOMCache = document.getElementById("dom-cache");
+
+let appHeader = document.getElementById("app-header");
+let appHeaderLeft = document.getElementById("app-header-left");
+let appHeaderRight = document.getElementById("app-header-right");
+
+let appContent = document.getElementById("app-content");
+
+let appSidebar = document.getElementById("app-sidebar");
+let appSidebarLink = document.getElementById("app-sidebar-link");
+let appSidebarLinkActive = document.getElementById("app-sidebar-link-active");
+
+let appMain = document.getElementById("app-main");
+
+let divAppPage = document.getElementById("page");
+
+let homePage = document.getElementById("home-page");
+let homePageImageContainer = document.getElementById("image-container");
+let ImageContainerHeader = document.getElementById("image-container-header");
+let imgHeaderTitle = document.getElementById("container-title");
+let imgHeaderSubtitle = document.getElementById("container-subtitle");
+let imageContainerSubHeader = document.getElementById("image-container-subheader");
+
+let homePageImageBox = document.getElementById("image-box");
+let imagePreview = document.getElementById("js--image-preview");
+let uploadOptions = document.getElementById("upload-options");
+let uploadField = document.getElementById("image-upload");
+
+
+let faceDetectionPage = document.getElementById("detection-page");
+
+let imageEnhancementPage = document.getElementById("enhancer-page");
