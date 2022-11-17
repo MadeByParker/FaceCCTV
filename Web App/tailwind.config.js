8f8b288113720d4fc652c71ace88ef13a3001bf7
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-11-17 10:19:56 +0000

    added dark mode

--- Web App/tailwind.config.js
+++ Web App/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+]
 }
