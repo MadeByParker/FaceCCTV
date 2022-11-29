add8e9744616d8335135519828f14e22b3951a00
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-11-29 01:39:07 +0000

    added jquery and other libraries

--- Web App/assets/js/index.js
+++ Web App/assets/js/index.js
@@ -1,10 +1,32 @@
-const button = document.querySelector('#menu-button');
-const menu = document.querySelector('#menu');
+// The app's platform ("web" or "app") depends on the ID of the document element.
+const appPlatform = document.documentElement.id;
 
+// In order to have the same codebase across the web and desktop app, there are a few empty variables that are only given a value when the app platform is set to "app".
+let electron = null;
+let ipcRenderer = null;
 
-button.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
+if(appPlatform !== "app" && typeof require === "undefined") {
+	var require = () => { 
+		return "";
+	};
+}
+
+let defaultSettings = {
+	"theme": "light"
+};
+
+const items = document.querySelectorAll(".app-sidebar-link");
+      items.forEach((item) => {
+        item.addEventListener("click", (e) => {
+          item.classList.toggle("active");
+          let itemsArray = [...items];
+          let array = itemsArray.filter((item) => item !== e.target);
+          array.forEach((elem) => {
+            //   to toggle hidden
+            elem.hidden = !elem.hidden;
+          });
+        });
+      });
 
 
 function dropDown() {
@@ -108,80 +130,3 @@ function dropDown() {
     }
   }
   
-
-  
-// The body element's ID is set to mobile or desktop depending on what device the user is on (based on the user agent).
-detectMobile() ? document.body.id = "mobile" : document.body.id = "desktop";
-
-// The app's platform ("web" or "app") depends on the ID of the document element.
-const appPlatform = document.documentElement.id;
-
-// In order to have the same codebase across the web and desktop app, there are a few empty variables that are only given a value when the app platform is set to "app".
-let electron = null;
-let ipcRenderer = null;
-
-if(appPlatform !== "app" && typeof require === "undefined") {
-	var require = () => { 
-		return "";
-	};
-}
-
-let defaultSettings = {
-	"theme": "light"
-};
-
-const container = document.querySelector(".container");
-const toggle = document.querySelector(".toggle");
-
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-// Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
-} else {
-    themeToggleDarkIcon.classList.remove('hidden');
-}
-
-var themeToggleBtn = document.getElementById('theme-toggle');
-
-themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-    // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
-    }
-    
-});
-
-
-
-if (
-  localStorage.getItem('color-theme') === 'dark' ||
-  (!('color-theme' in localStorage) &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches)
-) {
-  document.documentElement.classList.add('dark');
-} else {
-  document.documentElement.classList.remove('dark');
-}
