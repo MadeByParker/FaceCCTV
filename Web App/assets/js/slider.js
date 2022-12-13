a8bd2036d29452ec069ea10c8aa34a07011cc101
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-12-13 00:50:31 +0000

    Update slider.js

--- Web App/assets/js/slider.js
+++ Web App/assets/js/slider.js
@@ -1,4 +1,4 @@
 const container = document.querySelector('.slider-container');
 document.querySelector('.slider').addEventListener('input', (e) => {
-  container.style.setProperty('--position', `${e.target.value}%`);
+  container.style.setProperty('--position', `${e.target.value * 2}%`);
 })
