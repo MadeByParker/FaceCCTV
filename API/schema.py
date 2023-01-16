13fe55053759f8c3b5680442b7363aae0d8a35c8
Harry Parker <harry.parker@students.plymouth.ac.uk>
2023-01-16 13:21:36 +0000

    added django API

--- API/schema.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class Face(BaseModel):
-    face_detected: bool = False
--- API/tests/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:8000/upload/'
-file = {'file': open('10.jpg', 'rb')}
-resp = requests.post(url=url, files=file) 
-print(resp.json())
