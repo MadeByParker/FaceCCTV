14fde7c899fa4725c30b8b8b96675aa510054eed
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2023-01-22 14:31:07 +0000

    added new django API

--- API/FaceDetectionAPI/asgi.py
+++ API/FaceDetectionAPI/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FaceCCTVAPI project.
+ASGI config for FaceDetectionAPI project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FaceCCTVAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FaceDetectionAPI.settings')
 
 application = get_asgi_application()
--- API/FaceDetectionAPI/urls.py
+++ API/FaceDetectionAPI/urls.py
@@ -1,4 +1,4 @@
-"""FaceCCTVAPI URL Configuration
+"""FaceDetectionAPI URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 
 from django.contrib import admin
 from django.urls import path, include
