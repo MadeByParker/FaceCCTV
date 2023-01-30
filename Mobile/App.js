4ee3a5b6824fdfc81dda7f1484d282a47525e2ca
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2023-01-30 00:42:57 +0000

    Update App.js

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,51 +1,51 @@
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import React from "react";
 
 import Home from "./screens/Home";
-import About from "./screens/About";
 import FaceDetection from "./screens/FaceDetection";
 import ImageEnhancement from "./screens/ImageEnhancement";
 import Notifications from "./screens/Notifications";
 import Settings from "./screens/Settings";
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
+};
+
 const Stack = createStackNavigator();
 
-const theme = {
-      ...DefaultTheme,
-      colors: {
-            ...DefaultTheme.colors,
-            background: "transparent",
-      },
-      
-}
-
 const App = () => {
-      const [loaded] = useFonts({
-        InterBold: require("./assets/fonts/Inter-Bold.ttf"),
-        InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
-        InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
-        InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
-        InterLight: require("./assets/fonts/Inter-Light.ttf"),
-      });
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
 
-      if (!loaded) return null;
+  if (!loaded) return null;
 
-
-	return (
-            <NavigationContainer theme={theme}>
-                  <Stack.Navigator screenOptions={{ headerShown: false}} 
-                  initialRouteName="Home">
-                        <Stack.Screen name="Home" component={Home} />
-                        <Stack.Screen name="About" component={About} />
-                        <Stack.Screen name="Face Detection" component={FaceDetection} />
-                        <Stack.Screen name="Image Enhancement" component={ImageEnhancement} />
-                        <Stack.Screen name="Notifications" component={Notifications} />
-                        <Stack.Screen name="Settings" component={Settings} />
-                  </Stack.Navigator>
-            </NavigationContainer>
-	);
-}
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="ImageEnhancement" component={ImageEnhancement} />
+            <Stack.Screen name="Notifications" component={Notifications} />
+            <Stack.Screen name="Settings" component={Settings} />
+            
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
