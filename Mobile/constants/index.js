ef4ae9110c33677d73c955b7d5ab4c9f58f7562a
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2023-01-30 00:42:54 +0000

    added constants

---
+++ Mobile/constants/index.js
@@ -0,0 +1,5 @@
+import assets from './assets';
+import { COLORS, SIZES, FONTS, SHADOWS } from './theme';
+import { cardText } from './pages';
+
+export { assets, COLORS, SIZES, FONTS, SHADOWS, cardText };
