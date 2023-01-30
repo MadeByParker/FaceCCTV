209f8e4d37028ab3dcc6342a24cc72824940c6ff
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2023-01-30 00:42:39 +0000

    Update FaceDetection.js

--- Mobile/screens/FaceDetection.js
+++ Mobile/screens/FaceDetection.js
@@ -1,9 +1,57 @@
-import { Text } from 'react-native'
+import React from "react";
+import {
+	View,
+	SafeAreaView,
+	FlatList,
+	Text,
+	StatusBar,
+	Image,
+	ImageView,
+} from "react-native";
 
-const FaceDetection = () => {
-  return (
-    <Text>Face Detection</Text>
-  )
-}
+import { SIZES, assets } from "../constants";
+import { CircularButton, NormalButton, ActiveStatus } from "../components";
 
-export default FaceDetection
+const Header = ({ data, navigation }) => {
+	<View style={{ width: "100%", height: 375 }}>
+		<Image
+			source={data.image}
+			resizeMode="cover"
+			style={{ width: "100%", height: "100%" }}
+		/>
+		<CircularButton
+			imgURL={assets.back}
+			handlePress={() => navigation.goBack()}
+			left={15}
+			top={StatusBar.currentHeight + 15}
+		/>
+	</View>;
+};
+
+const Body = ({ route, navigation }) => {
+	const data = route.params;
+
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<ActiveStatus
+				barStyle="dark-content"
+				backgroundColor="transparent"
+				translucent={true}
+			/>
+			<View
+				style={{
+					width: "100%",
+					position: "absolute",
+					bottom: 0,
+					paddingVertical: SIZES.font,
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundColor: "rgba(255, 255, 255, 0.7)",
+					zIndex: 1,
+				}}
+			>
+				<NormalButton minWidth={200} fontSize={SIZES.large} />
+			</View>
+		</SafeAreaView>
+	);
+};
