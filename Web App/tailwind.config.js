f125f4aa135af92f22c60d731b33f63a5a13047f
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2023-02-17 00:30:20 +0000

    finished responsive

--- Web App/tailwind.config.js
+++ Web App/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./.html', ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+      content: ["./*.{html,js}"],
+      theme: {
+            screens: {
+                  xs: "350px",
+                  md: "1100px",
+            },
+            colors: {
+            'seablue': '#79bde9',
+            'blue': '#3d7bd5',
+            'darkblue': '#1a40c1',
+            'black': '#000000',
+            'white': '#ffffff',
+            'lightgray': '#393c41',
+            'gray': '#181c22',
+        extend: {},
+      },
+      plugins: [
+        require('flowbite/plugin'),
+        require('@tailwindcss/line-clamp'),
+    ]
+}
 }
