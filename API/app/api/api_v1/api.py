afaa82c63e9d022026da2e943dbf95ba3e2f1f1b
Harry Parker <harry.parker@students.plymouth.ac.uk>
2023-03-19 23:29:30 +0000

    moved api

--- API/app/api/api_v1/api.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from .endpoints import uploadfile
-
-router = APIRouter()
-router.include_router(uploadfile.router, tags=["Uploadfile"], prefix="/uploadfile")
--- API/app/api/api_v1/endpoints/print.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print(os.getcwd())
-
-os.listdir()
