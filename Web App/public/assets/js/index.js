2d1c1c2dc2a54e650a7cb0e13cdd41b21190fc3c
Harry Parker <harry.parker@students.plymouth.ac.uk>
2023-03-19 23:31:02 +0000

    added new navbar (works)

--- Web App/public/assets/js/index.js
+++ Web App/public/assets/js/index.js
@@ -4,9 +4,3 @@ function stripHTMLCharacters(string) {
 	string = replaceAll(string, ">", "&gt;");
 	return string;
 }
-
-
-function Menu(e){
-    let list = document.querySelector('ul');
-    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
-}
