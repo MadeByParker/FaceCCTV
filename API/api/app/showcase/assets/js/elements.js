130063c320e86288faf6b346878635e63a4a5e20
Harry Parker <72020025+Parker06@users.noreply.github.com>
2023-03-21 22:01:05 +0000

    added page

---
+++ API/api/app/showcase/assets/js/elements.js
@@ -0,0 +1,34 @@
+let buttonWindowClose = document.getElementById("button-window-close");
+let buttonWindowMinimize = document.getElementById("button-window-minimize");
+let buttonWindowMaximize = document.getElementById("button-window-maximize");
+
+let DOMCache = document.getElementById("dom-cache");
+
+let appContainer = document.getElementById('app-container');
+let appHeader = document.getElementById('app-header');
+let appHeaderLeft = document.getElementById('app-header-left');
+let appHeaderRight = document.getElementById('app-header-right');
+
+let navbarBrand = document.getElementById('navbar-brand');
+let navbarBrandIcon = document.getElementById('navbar-brand-img');
+let navbarTitle = document.getElementById('navbar-title');
+
+let themeToggleDiv = document.getElementById('theme-toggle');
+
+let appContent = document.getElementById('app-content');
+let appSidebar = document.getElementById('app-sidebar');
+let appSidebarLink = document.getElementById('app-sidebar-link');
+
+let appMain = document.getElementById('app-main');
+let homePage = document.getElementById('start-page');
+let imageContainer = document.getElementById('image-container');
+let imageContainerHeader = document.getElementById('image-container-header');
+let headerTitle = document.getElementById('container-title');
+let imageContainerSubHeader = document.getElementById('image-container-subheader');
+let subHeaderTitle = document.getElementById('container-subtitle');
+let dropzoneFile = document.getElementById('dropzone-file');
+
+let detectionPage = document.getElementById('detection-page');
+let imageEnhancementPage = document.getElementById('enhancer-page');
+let notificationPage = document.getElementById('notifications-page');
+let settingsPage = document.getElementById('settings-page');
