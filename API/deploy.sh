84f425b3f53f8d79e58bf77b6bd06b95dfba06bc
Harry Parker <72020025+Parker06@users.noreply.github.com>
2023-03-29 13:45:20 +0100

    added cuda

--- API/deploy.sh
+++ API/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 chmod u+x deploy.sh
+
+wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run
+sudo sh cuda_12.1.0_530.30.02_linux.run
+
 # This script is used to deploy the API to the server
 pip install --upgrade pip
 pip install -r requirements.txt
+pip install tensoRT
