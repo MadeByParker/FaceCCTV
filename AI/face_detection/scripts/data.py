a18ee483168981a90530faceaac6d1cee0dc27f2
Parker06 <harry.parker@students.plymouth.ac.uk>
2023-04-20 10:48:35 +0100

    deleted unnecessary python files

--- AI/face_detection/scripts/data.py
+++
@@ -1,8 +0,0 @@
-import face.config as config
-import face.data as data
-def main():
-      # Load data
-      data.DatasetBuilder(config.dataset_path).build_dataset()
-
-if __name__ == "__main__":
-      main()
