c0d49f958e6545ac4154a7d981ce19923d2a1b9a
Parker06 <harry.parker@students.plymouth.ac.uk>
2023-04-20 11:05:36 +0100

    updated API

--- API/main.py
+++ API/main.py
@@ -3,20 +3,15 @@ from fastapi.responses import StreamingResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import cv2
 import numpy as np
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageFilter
 import io
 import tensorflow as tf
 from keras.models import load_model
 import uvicorn
-from mangum import Mangum
-
 
 app = FastAPI()
-handler = Mangum(app)
 
-
-
-model = load_model('./models/facecctv.h5')
+model = load_model('./models/facecctv.h5', compile=False)
     
 @app.get("/")
 async def root():
@@ -25,70 +20,67 @@ async def root():
 # first 'static' specify route path, second 'static' specify html files directory.
 app.mount('/api', StaticFiles(directory='showcase',html=True))
 
-
 # Define a function to detect faces in an image
 def detect_faces(image):
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    resized = tf.image.resize(rgb, (240, 240))
-
-    faces = model.predict(np.expand_dims(resized/255,0))
-    # Create a copy of the original image
-    #image_copy = np.array(image)
-    # Draw bounding boxes around the faces
-    #for face in faces:
-        #x, y, w, h = face
-        #cv2.rectangle(image_copy, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-    # interpret the prediction
-    if faces[0] > 0.8:
-            # Create a copy of the original image
-        image_copy = np.array(image)
-        # Draw bounding boxes around the faces
-        for face in faces:
-            x, y, w, h = face
-            cv2.rectangle(image_copy, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        # Convert the image to bytes
-        image_bytes = io.BytesIO()
-        return Image.fromarray(image_copy) 
-    else:
-        return False
+    # Convert the image to RGB format
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # Resize the image to 300x300 pixels (the input size required by the SSD model)
+    resized_image = cv2.resize(rgb_image, (300, 300))
+    # Convert the image to a format that can be processed by the SSD model
+    input_data = np.expand_dims(resized_image, axis=0)
+    # Run the SSD model on the input data to detect faces
+    predictions = model.predict(input_data)
+    # Extract the bounding boxes and confidence scores from the predictions
+    boxes = predictions[0, :, :4]
+    scores = predictions[0, :, 4:]
+    # Select the bounding boxes with a confidence score above a threshold (e.g. 0.5)
+    threshold = 0.8
+    selected_boxes = boxes[scores > threshold]
     
-
+    # Draw bounding boxes around the selected faces
+    if len(selected_boxes) > 0:
+        for box in selected_boxes:
+            x1, y1, x2, y2 = box
+            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+    else:
+        # If no face is detected, return None
+        return None
+    # Return the image with bounding boxes around the detected faces
+    return image
 
 
 # Define an API endpoint to handle image uploads
 @app.post("/task/full-image-examination")
 async def full_image_examination(file: UploadFile = File(...)):
+# Read the image from the uploaded file
     contents = await file.read()
-    # Load the image from memory
     image = cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.IMREAD_COLOR)
     # Detect faces in the image
     result_image = detect_faces(image)
-    # enhance the image
-    if result_image != False:
-        byte_io = io.BytesIO()
-        result_image.save(byte_io, 'JPEG')
-        byte_io.seek(0)
-        return StreamingResponse(byte_io, media_type='image/jpeg', headers={'Content0-Disposition': 'attachment; filename=result.jpg'})
+    if result_image is not None:
+        # Convert the result image to bytes and return it as a response
+        result_image_bytes = cv2.imencode('.jpg', result_image)[1].tobytes()
+        return StreamingResponse(io.BytesIO(result_image_bytes), media_type='image/jpeg')
     else:
-        return {"message": "No face detected"}
+        # If no face is detected, return an error message
+        return {"message": "No face detected in the uploaded image."}
 
 
 # Define an API endpoint to handle image uploads
 @app.post("/task/face-detection")
 async def detect(file: UploadFile = File(...)):
+# Read the image from the uploaded file
     contents = await file.read()
-    # Load the image from memory
     image = cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.IMREAD_COLOR)
     # Detect faces in the image
     result_image = detect_faces(image)
-    if result_image != False:
-        byte_io = io.BytesIO()
-        result_image.save(byte_io, 'JPEG')
-        byte_io.seek(0)
-        return StreamingResponse(byte_io, media_type='image/jpeg', headers={'Content0-Disposition': 'attachment; filename=result.jpg'})
+    if result_image is not None:
+        # Convert the result image to bytes and return it as a response
+        result_image_bytes = cv2.imencode('.jpg', result_image)[1].tobytes()
+        return StreamingResponse(io.BytesIO(result_image_bytes), media_type='image/jpeg')
     else:
-        return {"message": "No face detected"}
+        # If no face is detected, return an error message
+        return {"message": "No face detected in the uploaded image."}
 
 # Define an API endpoint to handle image uploads
 @app.post("/task/image-enhancement")
@@ -96,9 +88,16 @@ async def enhance(file: UploadFile = File(...)):
     contents = await file.read()
     # Load the image from memory
     image = cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.IMREAD_COLOR)
-    # enhance the image
-    enhancer = ImageEnhance.Sharpness(Image)
-    enhanced_img = enhancer.enhance(2.0)
+    img = Image.open(image.file)
+
+    # Unblur the image using ImageFilter.UnsharpMask
+    unblurred_img = img.filter(ImageFilter.UnsharpMask(radius=2, percent=150, threshold=3))
+    # Convert to RGB color mode if not already in that mode
+    if unblurred_img.mode != "RGB":
+        unblurred_img = unblurred_img.convert("RGB")
+    # Improve the image quality using ImageEnhance.Sharpness
+    sharpness_enhancer = ImageEnhance.Sharpness(unblurred_img)
+    enhanced_img = sharpness_enhancer.enhance(2)
 
     # Convert the enhanced image to bytes
     enhanced_img_bytes = io.BytesIO()
@@ -108,7 +107,7 @@ async def enhance(file: UploadFile = File(...)):
         byte_io = io.BytesIO()
         enhanced_img.save(byte_io, 'JPEG')
         byte_io.seek(0)
-        return StreamingResponse(byte_io, media_type='image/jpeg', headers={'Content0-Disposition': 'attachment; filename=result.jpg'})
+        return StreamingResponse(byte_io.getvalue(), media_type='image/jpeg', headers={'Content0-Disposition': 'attachment; filename=result.jpg'})
     else:
         return {"message": "No image uploaded"}
 
