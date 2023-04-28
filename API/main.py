2b78892615c6d9a40828a3adf75cba96b5c745ca
Parker06 <harry.parker@students.plymouth.ac.uk>
2023-04-28 17:52:23 +0100

    API

--- API/main.py
+++ API/main.py
@@ -121,4 +121,4 @@ async def enhance(file: UploadFile = File(...)):
 
 # Start the FastAPI app
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
