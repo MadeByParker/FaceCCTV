e33ab64b7a4f3a54d2cdd36a7438413a982fc587
Parker06 <harry.parker@students.plymouth.ac.uk>
2023-05-06 17:38:58 +0100

    Update deploy.sh

--- API/deploy.sh
+++ API/deploy.sh
@@ -5,7 +5,28 @@ chmod u+x deploy.sh
 wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run
 sudo sh cuda_12.1.0_530.30.02_linux.run
 
-# This script is used to deploy the API to the server
+# Install production dependencies.
 pip install --upgrade pip
-pip install -r requirements.txt
-pip install nvidia-tensorrt
+pip install --no-cache-dir -r requirements.txt
+
+# Install the dependencies
+sudo apt-get install python3-pip python3-dev python3-venv
+sudo apt-get install libgl1-mesa-glx
+
+# Run individual install commands
+pip install fastapi --no-cache-dir
+pip install uvicorn --no-cache-dir
+pip install python-multipart --no-cache-dir
+pip install aiofiles --no-cache-dir
+pip install Pillow --no-cache-dir
+pip install opencv-python --no-cache-dir
+pip install scikit-image --no-cache-dir
+pip install scikit-learn --no-cache-dir
+pip install matplotlib --no-cache-dir
+pip install pandas --no-cache-dir
+pip install numpy --no-cache-dir
+pip install tensorflow --no-cache-dir
+pip install tqdm --no-cache-dir
+apt-get update -y
+apt-get install libgl1 -y
+apt-get install ffmpeg libsm6 libxext6  -y
