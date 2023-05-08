6925ef3e158c4fae627a2ec2113940a73a1a2405
Parker06 <harry.parker@students.plymouth.ac.uk>
2023-05-08 19:08:58 +0100

    added settings functions

--- Web App/public/assets/js/settings.js
+++ Web App/public/assets/js/settings.js
@@ -1,23 +1,71 @@
-const textSizeInput = document.getElementById('text-size');
+// Retrieve saved settings from localStorage (if any)
+const savedNavbarBackground = localStorage.getItem('navbarBackground');
+const savedAppBackground = localStorage.getItem('appBackground');
+const savedTextColor = localStorage.getItem('textColor');
+const savedTextSize = localStorage.getItem('textSize');
 
-textSizeInput.addEventListener('change', () => {
-  const textSize = textSizeInput.value;
-  localStorage.setItem('text-size', textSize);
-  setTextSize();
-});
+// Set the selected options based on the saved settings (if available)
+document.getElementById('settings-navbar').value = savedNavbarBackground || 'default';
+document.getElementById('settings-background-color').value = savedAppBackground || 'default';
+document.getElementById('settings-text-color').value = savedTextColor || 'white';
+document.getElementById('settings-text-size').value = savedTextSize || 'base';
 
-
-function setTextSize() {
-    const textSize = localStorage.getItem('text-size') || 'base';
-    const root = document.documentElement;
-    root.style.setProperty('--text-xs', `theme('fontSize.${textSize}.xs')`);
-    root.style.setProperty('--text-sm', `theme('fontSize.${textSize}.sm')`);
-    root.style.setProperty('--text-base', `theme('fontSize.${textSize}.base')`);
-    root.style.setProperty('--text-lg', `theme('fontSize.${textSize}.lg')`);
-    root.style.setProperty('--text-xl', `theme('fontSize.${textSize}.xl')`);
-    root.style.setProperty('--text-2xl', `theme('fontSize.${textSize}.2xl')`);
-    root.style.setProperty('--text-3xl', `theme('fontSize.${textSize}.3xl')`);
-    root.style.setProperty('--text-4xl', `theme('fontSize.${textSize}.4xl')`);
-    root.style.setProperty('--text-5xl', `theme('fontSize.${textSize}.5xl')`);
-    root.style.setProperty('--text-6xl', `theme('fontSize.${textSize}.6xl')`);
+// Function to apply the selected settings to the page
+function applySettings() {
+    const navbarBackground = document.getElementById('settings-navbar').value;
+    const appBackground = document.getElementById('settings-background-color').value;
+    const textColor = document.getElementById('settings-text-color').value;
+    const textSize = document.getElementById('settings-text-size').value;
+  
+    // Update navbar background
+    const navbar = document.getElementsByTagName('nav');
+    navbar.classList.remove('default', 'black-and-white', 'blue-and-pink', 'white', 'gray', 'black', 'dark-blue');
+    navbar.classList.add(navbarBackground);
+  
+    // Update app background
+    const app = document.body;
+    const slider = document.querySelector('.image-slider');
+    app.classList.remove('default', 'black-and-white', 'blue-and-pink', 'white', 'gray', 'black', 'dark-blue');
+    if (appBackground !== 'default') {
+        app.classList.add(appBackground);
+        slider.classList.add('invisible');
+    }
+    else {
+        slider.classList.remove('invisible');
+        app.classList.add('default');
+    }
+  
+    // Update text color
+    const textElements = document.body.querySelectorAll('h1, h2, h3, h4, h5, h6, p, a, li, span');
+    textElements.forEach((element) => {
+      element.classList.remove('text-white', 'text-black', 'text-gray');
+      element.classList.add(textColor);
+    });
+  
+    // Update text size
+    const textContainer = document.body;
+    textContainer.style.fontSize = textSize + 'rem';
   }
+
+// Function to handle changes in the settings
+function handleSettingsChange() {
+  const navbarBackground = document.getElementById('settings-navbar').value;
+  const appBackground = document.getElementById('settings-background-color').value;
+  const textColor = document.getElementById('settings-text-color').value;
+  const textSize = document.getElementById('settings-text-size').value;
+
+  // Save the settings to localStorage
+  localStorage.setItem('navbarBackground', navbarBackground);
+  localStorage.setItem('appBackground', appBackground);
+  localStorage.setItem('textColor', textColor);
+  localStorage.setItem('textSize', textSize);
+
+  // Apply the selected settings to the page
+  applySettings();
+}
+
+// Listen for changes in the settings and call the handleSettingsChange function
+document.getElementById('settings-navbar').addEventListener('change', handleSettingsChange);
+document.getElementById('settings-background-color').addEventListener('change', handleSettingsChange);
+document.getElementById('settings-text-color').addEventListener('change', handleSettingsChange);
+document.getElementById('settings-text-size').addEventListener('change', handleSettingsChange);
