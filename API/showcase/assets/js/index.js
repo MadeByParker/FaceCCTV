1dcbaf67e1e4cc80414fe89e5a9a13276348d725
Parker06 <harry.parker@students.plymouth.ac.uk>
2023-05-08 19:10:31 +0100

    removed showcase page from API routes

--- API/showcase/assets/js/index.js
+++
@@ -1,6 +0,0 @@
-// Converts HTML tags to avoid them being rendered. Prevents XSS attacks.
-function stripHTMLCharacters(string) {
-	string = replaceAll(string, "<", "&lt;");
-	string = replaceAll(string, ">", "&gt;");
-	return string;
-}
